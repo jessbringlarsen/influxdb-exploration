25ae6eee3dd05549b2cabdeb728ade6445f16b9d
Jess Bring-Larsen <jessbringlarsen@gmail.com>
2024-02-05 15:56:37 +0100

    Provide the admin token for convenience

--- telegraf-start.ps1
+++ telegraf-start.ps1
@@ -1,4 +1,4 @@
-$env:INFLUX_TOKEN=""
+$env:INFLUX_TOKEN="RQWlUg-S5QtgcPieJHU_WgWTDtDbqobm5nHF9txTxcpI9stRpg4yZCFp3hZmmna9Lnau-_Fn8haHO779iuujiw=="
 $env:INFLUX_HOST="http://localhost:8086"
 $env:INFLUX_ORG="test-org"
 $env:INFLUX_BUCKET="performance-bucket"
--- telegraf-start.sh
+++ telegraf-start.sh
@@ -1,4 +1,4 @@
-export INFLUX_TOKEN=""
+export INFLUX_TOKEN="RQWlUg-S5QtgcPieJHU_WgWTDtDbqobm5nHF9txTxcpI9stRpg4yZCFp3hZmmna9Lnau-_Fn8haHO779iuujiw=="
 export INFLUX_HOST="http://localhost:8086"
 export INFLUX_ORG="test-org"
 export INFLUX_BUCKET="performance-bucket"
